fc737a7d65bcb010347647a68c7578775ec0ebe1
McAsh09 <201951033@iiitvadodara.ac.in>
2021-06-09 14:30:03 +0530

    Website name added

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navbar.css';
+import Shopping from '../../shopping-main.jpg';
 
 
 function Navbar(){
@@ -9,11 +10,16 @@ function Navbar(){
       <div className="div">
           <nav className="nav">
               <ul>
+                  <li><a href="#">Carterio</a></li>
                   <li><a href="#">Add Items</a></li>
                   <li><a href="#">Checkout</a></li>
                   <li><a href="#">Contact Us</a></li>
               </ul>
           </nav>
+          
+          <div className="container">
+              <img className="container-image" src={Shopping} alt="shop-image" />
+          </div>
       </div>
 
     );
