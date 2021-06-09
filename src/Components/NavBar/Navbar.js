3a8981e382b4aea7af9d6621b02cbd9e6cd39e9c
McAsh09 <201951033@iiitvadodara.ac.in>
2021-06-09 13:46:48 +0530

    project files

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navbar.css';
+
+
+function Navbar(){
+
+    return(
+
+      <div className="div">
+          <nav className="nav">
+              <ul>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Work</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </nav>
+      </div>
+
+    );
+}
+
+export default Navbar;
